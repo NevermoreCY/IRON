46ce15e060ab574bffc2991324bb9f031ac9a5e5
YU CHEN <59667751+NevermoreCY@users.noreply.github.com>
2023-01-28 01:28:28 +0800

    update rain_scene

--- render_surface_train.sh
+++ render_surface_train.sh
@@ -1,14 +1,15 @@
 SCENE=$1
 
-
-python render_volume.py --mode train --conf ./confs/womask_iron.conf --case ${SCENE}
-
+echo "start rendering surface for train set"
 python render_surface.py --data_dir ./data_flashlight/${SCENE}/train \
                                  --out_dir ./exp_iron_stage2/${SCENE} \
                                  --neus_ckpt_fpath ./exp_iron_stage1/${SCENE}/checkpoints/ckpt_100000.pth \
                                  --num_iters 50001 --gamma_pred
+echo "end rendering surface for train set"
 # render test set
+echo "start rendering surface for test set"
 python render_surface.py --data_dir ./data_flashlight/${SCENE}/test \
                                  --out_dir ./exp_iron_stage2/${SCENE} \
                                  --neus_ckpt_fpath ./exp_iron_stage1/${SCENE}/checkpoints/ckpt_100000.pth \
                                  --num_iters 50001 --gamma_pred --render_all
+echo "end rendering surface for test set"
